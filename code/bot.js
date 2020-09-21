46a487e1a3cad0316cfcd3ada5ba141641ff43e7
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-09-21 09:13:11 +0300

    bot added

--- code/bot.js
+++ code/bot.js
@@ -1,1 +1,13 @@
+'use strict';
 
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, resp);
+});
