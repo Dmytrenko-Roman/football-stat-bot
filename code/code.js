7626e027f344a560527609be26a39a23202183cf
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-09-23 14:57:12 +0300

    fetch api added

--- code/code.js
+++ code/code.js
@@ -1,1 +1,12 @@
+const fetch = require('node-fetch');
 
+const body = { a: 1 };
+const url = "https://1xbet.whoscored.com/Players/119501/Show/Serge-Gnabry";
+
+fetch(url, {
+        method: 'post',
+        body:    JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json())
+    .then(json => console.log(json));
