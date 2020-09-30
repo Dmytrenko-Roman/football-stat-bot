cec387d331be81f4f83e2773cd2e6dd0fe3914da
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-09-30 11:37:25 +0300

    getting information

--- code/code.js
+++ code/code.js
@@ -1,11 +1,16 @@
 'use strict';
 
-$.ajax({
-    headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
-    url: 'http://api.football-data.org/v2/competitions/2021/standings',
-    dataType: 'json',
-    type: 'GET',
-  }).done(function(response) {
-    // do something with the response, e.g. isolate the id of a linked resource   
-    console.log(response);
+let result = {};
+const fetch = require('node-fetch');
+
+fetch('https://api.football-data.org/v2/competitions/SA/scorers', {
+  headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
+  dataType: 'json',
+  type: 'GET',
+})
+  .then(res => res.json())
+  .then(json => {
+    const name = json.scorers[0].player.name;
+    const goals = json.scorers[0].numberOfGoals;
+    console.log(name, goals);
   });
