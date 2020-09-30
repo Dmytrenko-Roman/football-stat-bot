9ea1755d104ec6eb0db3e1f2173e31efc4dbde01
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-09-30 11:19:13 +0300

    jquery deleted.

--- code/code.js
+++ code/code.js
@@ -1,8 +1,8 @@
 'use strict';
 
-match.ajax({
+$.ajax({
     headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
-    url: 'http://api.football-data.org/v2/matches?status='+ LIVE +'',
+    url: 'http://api.football-data.org/v2/competitions/2021/standings',
     dataType: 'json',
     type: 'GET',
   }).done(function(response) {
