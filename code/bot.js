c7385cfdd300f279f475ba6aa52acf58f3733564
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-10-03 18:12:25 +0300

    testing functions

--- code/bot.js
+++ code/bot.js
@@ -13,26 +13,17 @@ const bot = new TelegramBot(token, {
 
 bot.setWebHook(`${url}/bot${token}`);
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/topscorers/, msg => {
   const chatId = msg.chat.id;
-  const resp = match[1];
-  bot.sendMessage(chatId, resp);
-});
-
-/* const fetch = require('node-fetch');
-
-let name;
-let goals;
-
-fetch('https://api.football-data.org/v2/competitions/SA/scorers', {
+  fetch('https://api.football-data.org/v2/competitions/SA/scorers', {
   headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
   dataType: 'json',
   type: 'GET',
-})
-  .then(res => res.json())
-  .then(json => {
-    name = json.scorers[0].player.name;
-    goals = json.scorers[0].numberOfGoals;
-  });
-
-console.log(name, goals); */
+  })
+    .then(res => res.json())
+    .then(json => {
+      name = json.scorers[0].player.name;
+      goals = json.scorers[0].numberOfGoals;
+      bot.sendMessage(chatId, `${name}: ${goals}`);
+    });
+});
