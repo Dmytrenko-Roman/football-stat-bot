de798fb39dfe8696e02c4976f861367846c1a1a3
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-10-03 18:00:00 +0300

    decided to do bot in one file.

--- code/bot.js
+++ code/bot.js
@@ -11,3 +11,23 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   const resp = match[1];
   bot.sendMessage(chatId, resp);
 });
+
+'use strict';
+
+/* const fetch = require('node-fetch');
+
+let name;
+let goals;
+
+fetch('https://api.football-data.org/v2/competitions/SA/scorers', {
+  headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
+  dataType: 'json',
+  type: 'GET',
+})
+  .then(res => res.json())
+  .then(json => {
+    name = json.scorers[0].player.name;
+    goals = json.scorers[0].numberOfGoals;
+  });
+
+console.log(name, goals); */
