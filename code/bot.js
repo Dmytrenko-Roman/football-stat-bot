88e5764c60ef49c0ddb145448953ba7001fe8c1e
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-10 10:54:33 +0200

    refact

--- code/bot.js
+++ code/bot.js
@@ -2,7 +2,7 @@
 
 // Bot settings:
 
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
 const topScorers = require('./functions.js');
 
@@ -19,8 +19,10 @@ const bot = new TelegramBot(token, {
 
 bot.setWebHook(`${url}/bot${token}`);
 
-// Top scorers;
+// Top scorers:
 
+const names = [];
+const goals = [];
 const leagues = {
   SA: 'Serie A',
   PL: 'EPL',
@@ -43,7 +45,16 @@ bot.on('message', msg => {
     for (const k in leagues) {
       if (leagues[k] === t) league = `${k}`;
     }
-    topScorers(league, bot.sendMessage(chatId));
+    topScorers(league)
+      .then(json => {
+        const info = json.scorers;
+        for (let i = 0; i < info.length; i++) {
+          names[i] = info[i].player.name;
+          goals[i] = info[i].numberOfGoals;
+        }
+        const text = `1. ${names[0]}: ${goals[0]}\n2. ${names[1]}: ${goals[1]}\n3. ${names[2]}: ${goals[2]}\n4. ${names[3]}: ${goals[3]}\n5. ${names[4]}: ${goals[4]}\n6. ${names[5]}: ${goals[5]}\n7. ${names[6]}: ${goals[6]}\n8. ${names[7]}: ${goals[7]}\n9. ${names[8]}: ${goals[8]}\n10. ${names[9]}: ${goals[9]}`;
+        bot.sendMessage(chatId, text);
+      });
   }
 
   // Player's stats:
--- code/functions.js
+++ code/functions.js
@@ -2,27 +2,13 @@
 
 const fetch = require('node-fetch');
 
-
-function TopScorers(league, func) {
-  let text = '';
-  const names = [];
-  const goals = [];
-  fetch(`https://api.football-data.org/v2/competitions/${league}/scorers`, {
+function TopScorers(league) {
+  return fetch(`https://api.football-data.org/v2/competitions/${league}/scorers`, {
     headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
     dataType: 'json',
     type: 'GET',
   })
-    .then(response => response.json())
-    .then(json => {
-      const info = json.scorers;
-      for (let i = 0; i < info.length; i++) {
-        names[i] = info[i].player.name;
-        goals[i] = info[i].numberOfGoals;
-      }
-      text = `1. ${names[0]}: ${goals[0]}\n2. ${names[1]}: ${goals[1]}\n3. ${names[2]}: ${goals[2]}\n4. ${names[3]}: ${goals[3]}\n5. ${names[4]}: ${goals[4]}\n6. ${names[5]}: ${goals[5]}\n7. ${names[6]}: ${goals[6]}\n8. ${names[7]}: ${goals[7]}\n9. ${names[8]}: ${goals[8]}\n10. ${names[9]}: ${goals[9]}`;
-      //console.log(text);
-      func(par1, text);
-    });
+    .then(response => response.json());
 }
 
 //console.log(TopScorers('SA'));
