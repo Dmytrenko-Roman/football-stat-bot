54256aaf0452a10f357e44c0fb2f9e84cea74f43
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-10 10:11:54 +0200

    refactoring

--- code/bot.js
+++ code/bot.js
@@ -4,6 +4,7 @@
 
 const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
+const topScorers = require('./functions.js');
 
 const token = process.env.TOKEN;
 const url = process.env.APP_URL || 'https://playerstatbot.herokuapp.com/';
@@ -44,21 +45,7 @@ bot.on('message', msg => {
     for (const k in leagues) {
       if (leagues[k] === t) league = `${k}`;
     }
-    fetch(`https://api.football-data.org/v2/competitions/${league}/scorers`, {
-      headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
-      dataType: 'json',
-      type: 'GET',
-    })
-      .then(response => response.json())
-      .then(json => {
-        const info = json.scorers;
-        for (let i = 0; i < info.length; i++) {
-          names[i] = info[i].player.name;
-          goals[i] = info[i].numberOfGoals;
-        }
-        const text = `1. ${names[0]}: ${goals[0]}\n2. ${names[1]}: ${goals[1]}\n3. ${names[2]}: ${goals[2]}\n4. ${names[3]}: ${goals[3]}\n5. ${names[4]}: ${goals[4]}\n6. ${names[5]}: ${goals[5]}\n7. ${names[6]}: ${goals[6]}\n8. ${names[7]}: ${goals[7]}\n9. ${names[8]}: ${goals[8]}\n10. ${names[9]}: ${goals[9]}`;
-        bot.sendMessage(chatId, text);
-      });
+    topScorers(league);
   }
 
   // Player's stats:
--- code/functions.js
+++ code/functions.js
@@ -9,7 +9,16 @@ function TopScorers(league) {
     dataType: 'json',
     type: 'GET',
   })
-    .then(response => response.json());
+    .then(response => response.json())
+    .then(json => {
+      const info = json.scorers;
+      for (let i = 0; i < info.length; i++) {
+        names[i] = info[i].player.name;
+        goals[i] = info[i].numberOfGoals;
+      }
+      const text = `1. ${names[0]}: ${goals[0]}\n2. ${names[1]}: ${goals[1]}\n3. ${names[2]}: ${goals[2]}\n4. ${names[3]}: ${goals[3]}\n5. ${names[4]}: ${goals[4]}\n6. ${names[5]}: ${goals[5]}\n7. ${names[6]}: ${goals[6]}\n8. ${names[7]}: ${goals[7]}\n9. ${names[8]}: ${goals[8]}\n10. ${names[9]}: ${goals[9]}`;
+      bot.sendMessage(chatId, text);
+    });
 }
 
-//module.exports = TopScorers;
+module.exports = TopScorers;
