216afc5208202eef28848b283c04e5bd4077eab2
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-10 08:40:45 +0200

    refact

--- code/functions.js
+++ code/functions.js
@@ -4,12 +4,12 @@ const fetch = require('node-fetch');
 
 
 function TopScorers(league) {
-  fetch(`https://api.football-data.org/v2/competitions/${league}/scorers`, {
+  return fetch(`https://api.football-data.org/v2/competitions/${league}/scorers`, {
     headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
     dataType: 'json',
     type: 'GET',
   })
-    .then(res => res.json())
+    .then(response => response.json())
 }
 
 module.exports = TopScorers;
