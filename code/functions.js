44c8c74de6cbd476982d2607a723d6c559932929
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-10 08:35:36 +0200

    refactoring

--- code/functions.js
+++ code/functions.js
@@ -4,12 +4,12 @@ const fetch = require('node-fetch');
 
 
 async function TopScorers(league) {
-  const response = await fetch(`https://api.football-data.org/v2/competitions/${league}/scorers`, {
+  await fetch(`https://api.football-data.org/v2/competitions/${league}/scorers`, {
     headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
     dataType: 'json',
     type: 'GET',
-  });
-  response();
+  })
+    .then(res => res.json())
 }
 
 module.exports = TopScorers;
