d5a5e693fd37b24d3900f3e526e1f3d583d99f95
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-13 10:29:51 +0200

    refact

--- code/bot.js
+++ code/bot.js
@@ -27,8 +27,8 @@ const commands = {
 
 // Top scorers:
 
-const names = [];
-const goals = [];
+//const names = [];
+//const goals = [];
 const leagues = {
   SA: 'Serie A',
   PL: 'EPL',
@@ -52,14 +52,14 @@ bot.on('message', msg => {
 
   if (msgt.substr(0, commands.top.length) === commands.top) {
     const league = funcs.CheckLeague(leagues, msgt, commands.top);
+    let text;
     funcs.TopScorers(league)
       .then(json => {
         const info = json.scorers;
         for (let i = 0; i < info.length; i++) {
-          names[i] = info[i].player.name;
-          goals[i] = info[i].numberOfGoals;
+          text += `${i + 1}. ${info[i].player.name}: ${info[i].numberOfGoals}\n`;
         }
-        const text = `1. ${names[0]}: ${goals[0]}\n2. ${names[1]}: ${goals[1]}\n3. ${names[2]}: ${goals[2]}\n4. ${names[3]}: ${goals[3]}\n5. ${names[4]}: ${goals[4]}\n6. ${names[5]}: ${goals[5]}\n7. ${names[6]}: ${goals[6]}\n8. ${names[7]}: ${goals[7]}\n9. ${names[8]}: ${goals[8]}\n10. ${names[9]}: ${goals[9]}`;
+        //const text = `1. ${names[0]}: ${goals[0]}\n2. ${names[1]}: ${goals[1]}\n3. ${names[2]}: ${goals[2]}\n4. ${names[3]}: ${goals[3]}\n5. ${names[4]}: ${goals[4]}\n6. ${names[5]}: ${goals[5]}\n7. ${names[6]}: ${goals[6]}\n8. ${names[7]}: ${goals[7]}\n9. ${names[8]}: ${goals[8]}\n10. ${names[9]}: ${goals[9]}`;
         bot.sendMessage(chatId, text);
       });
   }
