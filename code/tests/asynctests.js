f6512231cea12f4d02ffcc67394ef90eaee576e5
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-16 11:01:56 +0200

    meet some errors

--- code/tests/asynctests.js
+++ code/tests/asynctests.js
@@ -5,17 +5,25 @@ console.log('Async tests:');
 {
   const fn = require('../functions.js');
 
-  const tests = [
-    ['PL', 'Top scorers (async): PL'],
-    ['PD', 'Top scorers (async): PD'],
-    ['BL1', 'Top scorers (async): BL1'],
-    ['FL1', 'Top scorers (async): FL1'],
-    ['CL', 'Top scorers (async): CL'],
-    ['SA', 'Top scorers (async): SA'],
+  const testsTopScorers = [
+    ['PL', 'Top scorers (async): PL |'],
+    ['PD', 'Top scorers (async): PD |'],
+    ['BL1', 'Top scorers (async): BL1 |'],
+    ['FL1', 'Top scorers (async): FL1 |'],
+    ['CL', 'Top scorers (async): CL |'],
+    ['SA', 'Top scorers (async): SA |'],
   ];
 
+  const testsTable = [
+    ['PL', 'Table (async): PL |'],
+    ['PD', 'Table (async): PD |'],
+    ['BL1', 'Table (async): BL1 |'],
+    ['FL1', 'Table (async): FL1 |'],
+    ['SA', 'Table (async): SA |'],
+  ]
+
   const AsyncConsoleLog = () => {
-    setTimeout(() => console.log(`Result: ${obj.t} tests, ${obj.p} passed, ${obj.f} failed`), 1000);
+    setTimeout(() => console.log(`Result: ${obj.t} tests, ${obj.p} passed, ${obj.f} failed`), 2000);
   }
 
   const obj = {
@@ -24,7 +32,7 @@ console.log('Async tests:');
     t: 0,
   };
 
-  for (const test of tests) {
+  for (const test of testsTopScorers) {
     const [par, name] = test;
       fn.TopScorers(par).then(json => {
         try {
@@ -42,5 +50,25 @@ console.log('Async tests:');
         }
       });
   }
+
+  /*for (const test2 of testsTable) {
+    const [par, name] = test2;
+      fn.Table(par).then(json => {
+        try {
+          obj.t++;
+          let text = '';
+          const tablejson = json.standings[0].table;
+          for (let k = 0; k < tablejson.length; k++) {
+            text += `${tablejson[k].position}. ${tablejson[k].team.name} |W:${tablejson[k].won}|D:${tablejson[k].draw}|L:${tablejson[k].lost}|P:${tablejson[k].points}|\n`;
+          }
+          obj.p++;
+          return text;
+        } catch (err) {
+          console.log(name, err);
+          obj.f++;
+        }
+      });
+  } */
+
   AsyncConsoleLog();
 }
