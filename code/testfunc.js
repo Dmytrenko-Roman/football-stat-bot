311127b38dea7723b8708cdb73375aae7e85dc49
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-19 10:57:23 +0200

    Update testfunc.js

--- code/testfunc.js
+++ code/testfunc.js
@@ -9,17 +9,5 @@ fetch('https://api.football-data.org//v2/matches', {
 })
   .then(response => response.json())
   .then(json => {
-    const matches = json.matches;
-    console.log(matches[0]);
-    for (let k = 0; k < matches.length; k++) {
-      const compName = matches[k].competition.name;
-      const homeTeam = matches[k].homeTeam.name;
-      const awayTeam = matches[k].awayTeam.name;
-      const score1 = matches[k].score.fullTime.homeTeam;
-      const score2 = matches[k].score.fullTime.awayTeam;
-      const date = matches[k].utcDate.substr(11, 5);
-
-      if (score1 !== null) console.log(`${compName}:\n${homeTeam} ${score1}:${score2} ${awayTeam}`);
-      console.log(`${compName}:\n${homeTeam} : ${awayTeam} | ${date} (Greenwich)`);
-    }
+    console.log(json.matches);
   });
