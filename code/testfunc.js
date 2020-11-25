33227ba1e205610b63d33d88afedb07013ac1572
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-25 15:22:19 +0200

    what is areas?

--- code/testfunc.js
+++ code/testfunc.js
@@ -2,38 +2,14 @@
 
 const fetch = require('node-fetch');
 
-const leaguesM = {
-  L1: ['Ligue 1', 'Ligue 1:\n'],
-  PL: ['Premier League', 'EPL:\n'],
-  BL: ['Bundesliga', 'Bundesliga:\n'],
-  SA: ['Serie A', 'Serie A:\n'],
-  LL: ['Primera Division', 'La Liga:\n'],
-};
-
-fetch('https://api.football-data.org//v2/matches', {
+fetch('https://api.football-data.org//v2/areas', {
   headers: { 'X-Auth-Token': '831ab788816b4517bdcf099d8cd99312' },
   dataType: 'json',
   type: 'GET',
 })
   .then(response => response.json())
   .then(json => {
-    const matches = json.matches;
-    for (let k = 0; k < matches.length; k++) {
-      const compName = matches[k].competition.name;
-      const homeTeam = matches[k].homeTeam.name;
-      const awayTeam = matches[k].awayTeam.name;
-      const score1 = matches[k].score.fullTime.homeTeam;
-      const score2 = matches[k].score.fullTime.awayTeam;
-      const date = matches[k].utcDate.substr(11, 5);
-      for (const key in leaguesM) {
-        if (compName === leaguesM[key][0]) {
-          if (score1 !== null) leaguesM[key][1] += `${homeTeam} ${score1}:${score2} ${awayTeam}\n`;
-          leaguesM[key][1] += `${homeTeam} : ${awayTeam} | ${date} (Greenwich)\n`;
-        }
-      }
-    }
-    const text = leaguesM.L1[1] + leaguesM.PL[1] + leaguesM.BL[1] + leaguesM.SA[1] + leaguesM.LL[1];
-    console.log(text);
+    console.log(json);
   });
 
 
