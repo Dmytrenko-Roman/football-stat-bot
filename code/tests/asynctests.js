59b34b5b5441960527d576d8ba2d997b12c2e426
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-26 18:53:05 +0200

    Table tests added.

--- code/tests/asynctests.js
+++ code/tests/asynctests.js
@@ -4,12 +4,16 @@ console.log('Async tests:');
 
 const fn = require('../functions.js')
 
-const params = ['BL1', 'SA', 'PL', 'CL', 'FL1', 'PD'];
+const params = ['BL1', 'SA', 'PL', 'FL1', 'PD', 'CL'];
 
 
 for (let i = 0; i < params.length; i++) {
   fn.TopScorers(params[i]).then(json => {
     if (json.error) console.log(`[TopScorers] Test "${params[i]}" is failed. Reason: ${json.message}`);
       else console.log(`[TopScorers] Test "${params[i]}" is passed.`);
+  });
+  fn.Table(params[i]).then(json => {
+    if (json.error) console.log(`[Table] Test "${params[i]}" is failed. Reason: ${json.message}`);
+      else console.log(`[Table] Test "${params[i]}" is passed.`);
   });
 }
