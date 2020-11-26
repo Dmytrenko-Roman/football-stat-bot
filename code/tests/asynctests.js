2f3d53d737eaa6be5e69e9785bf463e3e03bda0e
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-26 18:45:59 +0200

    Update asynctests.js

--- code/tests/asynctests.js
+++ code/tests/asynctests.js
@@ -2,73 +2,18 @@
 
 console.log('Async tests:');
 
-{
-  const fn = require('../functions.js');
+const fn = require('../functions.js')
 
-  const testsTopScorers = [
-    ['PL', 'Top scorers (async): PL |'],
-    ['PD', 'Top scorers (async): PD |'],
-    ['BL1', 'Top scorers (async): BL1 |'],
-    ['FL1', 'Top scorers (async): FL1 |'],
-    ['CL', 'Top scorers (async): CL |'],
-    ['SA', 'Top scorers (async): SA |'],
-  ];
+const params = ['BL1'];
 
-  const testsTable = [
-    ['PL', 'Table (async): PL |'],
-    ['PD', 'Table (async): PD |'],
-    ['BL1', 'Table (async): BL1 |'],
-    ['FL1', 'Table (async): FL1 |'],
-    ['SA', 'Table (async): SA |'],
-  ]
 
-  const AsyncConsoleLog = () => {
-    setTimeout(() => console.log(`Result: ${obj.t} tests, ${obj.p} passed, ${obj.f} failed`), 2000);
-  }
-
-  const obj = {
-    p: 0,
-    f: 0,
-    t: 0,
-  };
-
-  for (const test of testsTopScorers) {
-    const [par, name] = test;
-      fn.TopScorers(par).then(json => {
-        try {
-          obj.t++;
-          const info = json.scorers;
-          let text = '';
-          for (let i = 0; i < info.length; i++) {
-            text += `${i + 1}. ${info[i].player.name}: ${info[i].numberOfGoals}\n`;
-          }
-          obj.p++;
-          return text;
-        } catch (err) {
-          console.log(name, err);
-          obj.f++;
-        }
-      });
-  }
-
-  for (const test of testsTable) {
-    const [par, name] = test;
-      fn.Table(par).then(json => {
-        try {
-          obj.t++;
-          let text = '';
-          const tablejson = json.standings[0].table;
-          for (let k = 0; k < tablejson.length; k++) {
-            text += `${tablejson[k].position}. ${tablejson[k].team.name} |W:${tablejson[k].won}|D:${tablejson[k].draw}|L:${tablejson[k].lost}|P:${tablejson[k].points}|\n`;
-          }
-          obj.p++;
-          return text;
-        } catch (err) {
-          console.log(name, err);
-          obj.f++;
-        }
-      });
-  }
-
-  AsyncConsoleLog();
+for (let i = 0; i < params.length; i++) {
+  fn.TopScorers(params[i]).then(json => {
+    if (json.error) {
+      console.log(`[TopScorers] Test "${params[i]}" is failed. Reason: ${json.message}`);
+    } else {
+      console.log(json);
+      console.log(`[TopScorers] Test "${params[i]}" is passed.`);
+    }
+    });
 }
