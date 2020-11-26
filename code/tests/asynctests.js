a752a7a3f6dd80bcf3a2854f39496313189b8715
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-11-26 18:48:42 +0200

    tests are working!!!

--- code/tests/asynctests.js
+++ code/tests/asynctests.js
@@ -4,16 +4,12 @@ console.log('Async tests:');
 
 const fn = require('../functions.js')
 
-const params = ['BL1'];
+const params = ['BL1', 'SA', 'PL', 'CL', 'FL1', 'PD'];
 
 
 for (let i = 0; i < params.length; i++) {
   fn.TopScorers(params[i]).then(json => {
-    if (json.error) {
-      console.log(`[TopScorers] Test "${params[i]}" is failed. Reason: ${json.message}`);
-    } else {
-      console.log(json);
-      console.log(`[TopScorers] Test "${params[i]}" is passed.`);
-    }
-    });
+    if (json.error) console.log(`[TopScorers] Test "${params[i]}" is failed. Reason: ${json.message}`);
+      else console.log(`[TopScorers] Test "${params[i]}" is passed.`);
+  });
 }
