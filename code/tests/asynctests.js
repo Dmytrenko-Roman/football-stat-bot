6ec1d2af88050915aa188e22e69ec5f60d1db531
KERRIGAN <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-01-09 10:45:41 +0200

    Update asynctests.js

--- code/tests/asynctests.js
+++ code/tests/asynctests.js
@@ -6,7 +6,6 @@ const fn = require('../functions.js')
 
 const params = ['BL1', 'SA', 'PL', 'FL1', 'PD', 'CL'];
 
-
 for (let i = 0; i < params.length; i++) {
   fn.TopScorers(params[i]).then(json => {
     if (json.error) console.log(`[TopScorers] Test "${params[i]}" is failed. Reason: ${json.message}`);
