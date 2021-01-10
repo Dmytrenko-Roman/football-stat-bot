3782529d6de3f860ec5451420146abd07abb5ab1
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-01-10 09:26:24 +0200

    tests fixed

--- code/tests/asynctests.js
+++ code/tests/asynctests.js
@@ -2,21 +2,21 @@
 
 console.log('Async tests:');
 
-const fn = require('../functions.js')
+const fn = require('../functions.js');
 
 const params = ['BL1', 'SA', 'PL', 'FL1', 'PD', 'CL'];
 
 for (let i = 0; i < params.length; i++) {
   fn.TopScorers(params[i]).then(json => {
-    if (json.error) console.log(`[TopScorers] Test "${params[i]}" is failed. Reason: ${json.message}`);
-      else console.log(`[TopScorers] Test "${params[i]}" is passed.`);
+    if (json.error) throw `[TopScorers] Test "${params[i]}" is failed. Reason: ${json.message}`;
+    else console.log(`[TopScorers] Test "${params[i]}" is passed.`);
   });
   fn.Table(params[i]).then(json => {
-    if (json.error) console.log(`[Table] Test "${params[i]}" is failed. Reason: ${json.message}`);
-      else console.log(`[Table] Test "${params[i]}" is passed.`);
+    if (json.error) throw `[Table] Test "${params[i]}" is failed. Reason: ${json.message}`;
+    else console.log(`[Table] Test "${params[i]}" is passed.`);
   });
   fn.Matches(params[i]).then(json => {
-    if (json.error) console.log(`[Matches] Test "${params[i]}" is failed. Reason: ${json.message}`);
-      else console.log(`[Matches] Test "${params[i]}" is passed.`);
+    if (json.error) throw `[Matches] Test "${params[i]}" is failed. Reason: ${json.message}`;
+    else console.log(`[Matches] Test "${params[i]}" is passed.`);
   });
 }
